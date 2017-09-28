12ad6867d269ceca05c0ec91ee1de16928f44c91
EJ Perez <idyey04@gmail.com>
2017-09-28 14:03:15 +0800

    Updated precached files

--- sw.js
+++ sw.js
@@ -3,7 +3,12 @@
 importScripts('sw-toolbox.js');
 
 toolbox.precache([
-	'index.html'
+	'index.html',
+	'dist/css/styles.min.css',
+	'dist/css/print.min.css',
+	'dist/js/scripts.min.css',
+	'dist/fonts/raleway-v11-latin-regular.woff2',
+	'dist/fonts/inconsolata-v15-latin-regular.woff2'
 ]);
 	
 // toolbox.router.get('/images/*', toolbox.cacheFirst);
